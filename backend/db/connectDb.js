0a6e6f149074851a5ee7d789da0c1f139f3fb7f7
sadafshahab12 <sadafshahab123@yahoo.com>
2025-07-21 09:48:22 +0500

    learning crud with mern

---
+++ backend/db/connectDb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoDbUrl = process.env.LOCAL_MONGO_URI;
+
+export const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(mongoDbUrl);
+    console.log(`Mongodb connect successfully : ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Mongodb connection failed : ${error}`);
+  }
+};
